08d27a7eb2cfbc9f9c8dc0ceab9ff76ac9b12c78
JerickTwO <jerickjjtm774@gmail.com>
2025-07-24 19:30:23 -0500

    add

--- src/main/java/org/aprendiendo/deploy/DeployApplication.java
+++ src/main/java/org/aprendiendo/deploy/DeployApplication.java
@@ -2,9 +2,17 @@ package org.aprendiendo.deploy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DeployApplication {
+
+    @GetMapping("/mensaje")
+    public String mensaje() {
+        return "Bienvenido a Java + Azure";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DeployApplication.class, args);
