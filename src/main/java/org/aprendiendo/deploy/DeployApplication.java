75b165597e2e89de18900f8d34c72ccbba406291
JerickTwO <jerickjjtm774@gmail.com>
2025-07-24 19:22:27 -0500

    add deploy

--- src/main/java/org/aprendiendo/deploy/DeployApplication.java
+++ src/main/java/org/aprendiendo/deploy/DeployApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DeployApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DeployApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DeployApplication.class, args);
+    }
 
 }
--- src/test/java/org/aprendiendo/deploy/DeployApplicationTests.java
+++ src/test/java/org/aprendiendo/deploy/DeployApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DeployApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
